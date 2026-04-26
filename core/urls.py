64723a84354be1fd5a271d00b687e73422b828b6
Prince Singh <princesingh83778@gmail.com>
2026-04-27 04:52:50 +0530

    fix trailing slashes

--- core/urls.py
+++ core/urls.py
@@ -4,20 +4,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),    
-    path('settings', views.settings, name='settings'),
-    path('upload', views.upload, name='upload'),
-    path('delete-post', views.delete_post, name='delete_post'),
-    path('report-post', views.report_post, name='report_post'),
-    path('favourite', views.favourite_post, name='favourite'),
-    path('follow', views.follow, name='follow'),
-    path('search', views.search, name='search'),
-    path('search-ajax', views.search_ajax, name='search_ajax'),
-    path('profile/<str:pk>', views.profile, name='profile'),
-    path('like-post', views.like_post, name='like-post'),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
-    path('logout', views.logout, name='logout'),
-    
+    path('settings/', views.settings, name='settings'),
+    path('upload/', views.upload, name='upload'),
+    path('delete-post/', views.delete_post, name='delete_post'),
+    path('report-post/', views.report_post, name='report_post'),
+    path('favourite/', views.favourite_post, name='favourite'),
+    path('follow/', views.follow, name='follow'),
+    path('search/', views.search, name='search'),
+    path('search-ajax/', views.search_ajax, name='search_ajax'),
+    path('profile/<str:pk>/', views.profile, name='profile'),
+    path('like-post/', views.like_post, name='like-post'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('logout/', views.logout, name='logout'),
     
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
