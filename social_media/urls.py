fa45a470dda3f9c0aa2cc2d79e52755e4dd25ff3
Prince Singh <princesingh83778@gmail.com>
2026-04-27 04:41:22 +0530

    fix root url

--- social_media/urls.py
+++ social_media/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.urls'))
+    path('',include('core.urls')),
+    path('', lambda request: redirect('/signin'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
